1000978b98928f03700902839db12af1ee80f08f
rafat1515 <113086563+rafat1515@users.noreply.github.com>
2022-09-15 18:00:08 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+	static public void main(String args[])
+	{
+		int a=30000;
+		int b=11000;
+		int c=5000;
+		int z=a-(b+c);
+		
+		System.out.println("Salary of the third employee:"  +z);
+	}
+}
