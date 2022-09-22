dc131fe2b9c9a3bcbfb0c4368093efad507917a8
rafat1515 <113086563+rafat1515@users.noreply.github.com>
2022-09-22 18:04:50 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,20 @@
+class Bank extends BankInfo {
+	
+
+	void clientId()
+	{
+		super.clientId();
+		int A = 10001;
+		System.out.println(" Client Id is : "+ a);
+		
+	}
+	void clientName()
+	{
+		super.clientName();
+		int A = Rafat Shaikh;
+		System.out.println(" Client Name is : "+ a);
+		
+	}
+		public static void main(String[] args) 
+		{
+}
