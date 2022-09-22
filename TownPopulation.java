b57a38f8f2ec0177f995358f2352f4c3370a3d8e
rafat1515 <113086563+rafat1515@users.noreply.github.com>
2022-09-22 17:53:03 +0530

    Add files via upload

---
+++ TownPopulation.java
@@ -0,0 +1,16 @@
+package com.OOBs;
+
+public class TownPopulation {
+
+	public static void main(String[] args) 
+	{
+		int x = 262500;
+		int y = 175000;
+		int z = x - y;
+		int a = (z*100) / 175000;
+		int b = a/10;
+		System.out.println(" The Average percent increase of population per year is : " + b + " % ");
+		
+	}
+
+}
